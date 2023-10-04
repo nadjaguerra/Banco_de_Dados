81e83129a690f5884cd5b6b59031499d0f367477
Nadja Guerra <112482228+nadjaguerra@users.noreply.github.com>
2023-10-04 16:54:20 -0300

    cadastro simples.sql

--- cadastro simples.sql
+++ cadastro simples.sql
@@ -1,4 +1,4 @@
-/* Modelagem Básica => ENTIDADE = TABELA
+/* Modelagem Básica 
 O cliente Tera Comércio de Produtos S.A, solicitou a modelagem de um banco de dados para cadastro dos seus clientes.
 A função da 'Unidados' é a análise dos requisitos junto aos usuários para a correta construção do produto. O cliente deseja que inicialmente os scripts sejam construídos 
 para o Banco de Dados MySQL, porém, posteriormente pode haver mudança no ambiente e consequentemente adaptação dos scripts para outros produtos de SGBD.
