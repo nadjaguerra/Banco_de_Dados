a6bb4d71215d92d41e1735235c2b7beaac4f22ff
Nadja Guerra <112482228+nadjaguerra@users.noreply.github.com>
2023-10-04 18:46:24 -0300

    cadastro simples.sql

--- cadastro simples.sql
+++ cadastro simples.sql
@@ -1,4 +1,4 @@
-/* Modelagem Básica 
+/* Modelagem Básica
 O cliente Tera Comércio de Produtos S.A, solicitou a modelagem de um banco de dados para cadastro dos seus clientes.
 A função da 'Unidados' é a análise dos requisitos junto aos usuários para a correta construção do produto. O cliente deseja que inicialmente os scripts sejam construídos 
 para o Banco de Dados MySQL, porém, posteriormente pode haver mudança no ambiente e consequentemente adaptação dos scripts para outros produtos de SGBD.
@@ -13,8 +13,8 @@ O cliente não quer nenhuma informação relativa à vendas ou estoque, desejand
 
 /* CRIANDO BANCO DE DADOS */
 
-CREATE DATABASE PROJETOOOO;
-USE PROJETOOOO;
+CREATE DATABASE PROJEETOOO;
+USE PROJEETOOO;
 
 /* CRIANDO A TABELA DE CLIENTES */
 CREATE TABLE CLIENTE(
@@ -28,16 +28,37 @@ CREATE TABLE CLIENTE(
 
 INSERT INTO CLIENTE (NOME, CPF, EMAIL, TELEFONE, ENDERECO, SEXO)
 VALUES
-    ('ANA', '12345678901', 'ANA@GLOBO.COM', '85548962', 'PRES ANTONIO CARLOS - CENTRO - SAO PAULO - SP', 'F'),
+    ('ANA', '12345678901', 'ANA@GLOBO.COM', '85548962', 'AV. CONDE DA BOA VISTA - BOA VISTA - RECIFE - PE', 'F'),
     ('LILIAN', '23456789012', 'LILIAN@GMAIL.COM', '947785696', NULL, 'F'),
-    ('CARLA', '34567890123', 'CARLA@TERATI.COM.BR', '7745828', 'SAMUEL SILVA - CENTRO - BELO HORIZONTE - MG', 'F');
+    ('OTTO', '12547896547', 'OTTO@GMAIL.COM', '999478564', 'AV. POLIDORO - VARZEA - RECIFE - PE', 'M'),
+    ('CARLA', '34567890123', 'CARLA@TERATI.COM.BR', '7745828', 'SAMUEL SILVA - CENTRO - BELO HORIZONTE - MG', 'F'),
+    ('LUIZ', '48756932145', 'LUIZZ@GMAIL.COM', '944567789', NULL ,'M');
 
 -- Descreve a estrutura da tabela "CLIENTE"
 DESCRIBE CLIENTE;
 
 
 -- Exibe os registros restantes na tabela "CLIENTE"
+
 SELECT NOME, CPF, EMAIL, TELEFONE, ENDERECO, SEXO, NOW() AS DATA_HORA FROM CLIENTE;
+
+SELECT NOME, CPF FROM CLIENTE
+WHERE SEXO = 'M';
+
+SELECT NOME, CPF FROM CLIENTE
+WHERE SEXO = 'F';
+
+SELECT NOME, ENDERECO FROM CLIENTE
+WHERE ENDERECO LIKE '%PE';
+
+UPDATE CLIENTE
+SET ENDERECO = 'RUA TITO ROSAS - PARNAMIRIM - RECIFE - PE'
+WHERE NOME = 'LILIAN' AND CPF = '23456789012';
+
+INSERT INTO CLIENTE (NOME, CPF, EMAIL, TELEFONE, ENDERECO, SEXO)
+VALUES
+    ('TITO', '12836738901', 'TITO@GMAIL.COM', '865478962', 'RUA SIGISMUNDO GONÇALVES - CORDEIRO - RECIFE - PE', 'M');
+
 
 
 
